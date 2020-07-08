a4e3a89fefe42fd0e1887bb61487f519c581fccf
Jan Molak <jan.molak@smartcodeltd.co.uk>
2020-07-08 11:23:34 +0100

    Default Chrome to headless

--- exercises/07-ui/protractor.conf.js
+++ exercises/07-ui/protractor.conf.js
@@ -68,7 +68,9 @@ exports.config = {
                 '--log-level=3',
                 '--disable-gpu',
                 '--window-size=1920,1080',
-            ].concat(isCI ? ['--headless'] : [])    // run in headless mode on the CI server
+                '--headless',
+            ]
+            //.concat(isCI ? ['--headless'] : [])    // run in headless mode on the CI server
         }
     }
 };
